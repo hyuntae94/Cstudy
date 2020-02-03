a621e45a2ad0d8ef8afe8b05acaf61d9a2f9e7a7
hyuntae94 <believe4279@gmail.com>
2020-02-04 02:00:21 +0900

    재귀함수

---
+++ rotate.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+static void swap(int** a, int** b)
+{
+	int* temp;
+	temp = **a;
+	**a = **b;
+	**b = temp;
+}
+static void rotate(int* a, int* b, int* c,int* d)
+{
+	char enter;
+	printf("%d : %d : %d : %d", *a, *b, *c, *d);
+	scanf("%c", &enter);
+	do
+	{
+		if (enter == '\n')
+		{
+			swap(&a, &b);
+			swap(&b, &c);
+			swap(&c, &d);
+			printf("%d : %d : %d : %d", *a, *b, *c, *d);
+			scanf("%c", &enter); //값입력
+		}
+	} while (enter == '\n');
+}
+
+/*재귀함수를 이용한 방법*/
+
+/*void rotate(int* pa,int* pb,int* pc) 
+{
+	printf("%d : %d : %d", *pa, *pb, *pc);
+		
+	while (1) 
+	{
+		char ch = getchar();
+		
+		if (ch == '\n') 
+		{
+			rotate(pb, pc, pa); 
+		}
+		else exit(0);
+	}
+}*/
+
+int main(void)
+{
+	int a = 1, b = 2, c = 3, d = 4;
+	rotate(&a, &b, &c, &d);
+	return 0;
+}
