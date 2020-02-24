849e57efe42d0774267d0da89a14ae2f9be45ee7
hyuntae94 <believe4279@gmail.com>
2020-02-24 14:45:34 +0900

    파일입출력

--- fprintf,fscanf.c
+++ fprintf,fscanf.c
@@ -23,13 +23,13 @@ int main(void)
 		printf("파일을 열지 못했습니다.\n");
 		return 1;
 	}
-	res = fscanf(ifp, "%s%d%d%d", &name, &kor, &eng, &math);
+	res = fscanf(ifp, "%s%d%d%d", name, &kor, &eng, &math);
 	while (res != -1)
 	{
 		tot = kor + eng + math;
 		avg = tot / 3.0;
 		fprintf(ofp, "%s%5d%7.1lf\n", name, tot, avg);
-		res = fscanf(ifp, "%s%d%d%d", &name, &kor, &eng, &math);
+		res = fscanf(ifp, "%s%d%d%d", name, &kor, &eng, &math);
 	}
 	fclose(ifp);
 	fclose(ofp);
